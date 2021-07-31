970b165ac5e06d6118b96a4f3e5b736b19f41463
yashroher <roherayash.drive@gmail.com>
2021-07-31 17:45:27 +0530

    -

--- src/components/sections/FeedBack.js
+++ src/components/sections/FeedBack.js
@@ -5,18 +5,18 @@ const openFeedBackForm = () => {
     var display = document.getElementById("FeedBack").style.display;
     if(display == "flex"){
         document.getElementById("FeedBack").style.display = "none";
-        document.getElementById("FeedBack__btn").innerHTML = "Feedback";
+        document.getElementById("FeedBack__btn").innerHTML = "FEEDBACK";
     } 
     else{
         document.getElementById("FeedBack").style.display = "flex";
-        document.getElementById("FeedBack__btn").innerHTML = "Close";
+        document.getElementById("FeedBack__btn").innerHTML = "CANCEL";
     }
 }
 
 const FeedBack = () => (
     <div className = "FeedBack">
         <button id = "FeedBack__btn" onClick = {openFeedBackForm} className = "FeedBack__btn">
-            Feedback
+            FEEDBACK
         </button>
         <FeedBackForm />
     </div>
