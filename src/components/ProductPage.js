c7619bc561a87d521215413b41e229b36bb0f508
yashroher <roherayash.drive@gmail.com>
2021-08-30 00:02:43 +0530

    Product Page Version 1.0

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -8,75 +8,77 @@ import purchase from '../../public/img/CompanyRelated/purchase_year.svg';
 import insurance from '../../public/img/CompanyRelated/insurance.svg';
 import transmission_type from '../../public/img/CompanyRelated/transmission_type.svg';
 
-const ProductPage = () => (
-    <div className = "ProductPage">
-        <div className = "Car-Options">
-            <div className = "Option">
-                <div className = "Car-Img">
-                    <img src = {car}/>
+export default class ProductPage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            isOpen = false,
+        }
+    }
+    render(){
+        return (
+            <div className = "ProductPage">
+            <div className = "Car-Options">
+                <div className = "Option">
+                    <div className = "Car-Img">
+                        <img src = {car}/>
+                    </div>
+                    <div className = "Car-Name">
+                        <h1>Maruti Suzuki Wagon R</h1>
+                    </div>
+                    <div className = "Car-Details">
+                        <div className = "details">
+                            <div>
+                                <img src = {fuel_type}/>
+                            </div>
+                            <div>
+                                <p>Diesel</p>
+                            </div>
+                        </div>
+                        <div className = "details">
+                            <div>
+                                <img src = {km_driven}/>
+                            </div>
+                            <div>
+                                <p>2,000,000 km</p>
+                            </div>
+                        </div>
+                        <div className = "details">
+                            <div>
+                                <img src = {transmission_type}/>
+                            </div>
+                            <div>
+                                <p>Manual</p>
+                            </div>
+                        </div>
+                        <div className = "details">
+                            <div>
+                                <img src = {purchase}/>
+                            </div>
+                            <div>
+                                <p>Jan 2021</p>
+                            </div>
+                        </div>
+                        <div className = "details">
+                            <div>
+                                <img src = {Owner}/>
+                            </div>
+                            <div>
+                                <p>2nd Owner</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className = "Car-Name">
-                    <h1>Maruti Suzuki Wagon R</h1>
-                </div>
-                <div className = "Car-Details">
-                    <div className = "details">
-                        <div>
-                            <img src = {fuel_type}/>
-                        </div>
-                        <div>
-                            <p>Diesel</p>
-                        </div>
-                    </div>
-                    <div className = "details">
-                        <div>
-                            <img src = {km_driven}/>
-                        </div>
-                        <div>
-                            <p>2,000,000 km</p>
-                        </div>
-                    </div>
-                    <div className = "details">
-                        <div>
-                            <img src = {insurance}/>
-                        </div>
-                        <div>
-                            <p>Valid Upto Jan 2023 Third_party</p>
-                        </div>
-                    </div>
-                    <div className = "details">
-                        <div>
-                            <img src = {transmission_type}/>
-                        </div>
-                        <div>
-                            <p>Manual</p>
-                        </div>
-                    </div>
-                    <div className = "details">
-                        <div>
-                            <img src = {purchase}/>
-                        </div>
-                        <div>
-                            <p>Jan 2021</p>
-                        </div>
-                    </div>
-                    <div className = "details">
-                        <div>
-                            <img src = {Owner}/>
-                        </div>
-                        <div>
-                            <p>2nd Owner</p>
-                        </div>
-                    </div>
-                </div>
+                <div className = "Option"></div>
+                <div className = "Option"></div>
+                <div className = "Option"></div>
+                <div className = "Option"></div>
+                <div className = "Option"></div>
+                <div className = "Option"></div>         
             </div>
-            <div className = "Option"></div>
-            <div className = "Option"></div>
-            <div className = "Option"></div>
-            <div className = "Option"></div>
-            <div className = "Option"></div>
-            <div className = "Option"></div>         
         </div>
-    </div>
-);
+        )
+    }
 
-export default ProductPage;
+};
+
